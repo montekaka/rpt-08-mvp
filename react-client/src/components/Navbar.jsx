da4d440786fbf7195501160c585e268a76038303
montekaka <josh.chen.jia@gmail.com>
2018-08-08 18:16:00 -0700

    add boostrap style and navbar component

---
+++ react-client/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a className="navbar-brand" href="#">Product Name</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav mr-auto">
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    )
+  }   
+}
+
+export default Navbar;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import $ from 'jquery';
 import List from './components/List.jsx';
+import Navbar from './components/Navbar.jsx'
 import Websites from './components/Websites.jsx';
 import Search from './components/Search.jsx';
 import Website from './components/Website.jsx';
@@ -18,13 +19,13 @@ class App extends React.Component {
 
   render () {
     return (
-      <Router>
-        <div>
-          <h1>Item List</h1>         
+      <Router>        
+        <span>
+          <Navbar></Navbar>
           <Route exact path="/" component={Search} ></Route>
           <Route exact path="/" component={Websites} ></Route>
           <Route path="/websites/:_id" component={Website} />     
-        </div>        
+        </span>        
       </Router>
 )
   }
