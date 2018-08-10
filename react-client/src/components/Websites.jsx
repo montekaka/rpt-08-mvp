9802a74162053b78884a05cb75c811626418f804
montekaka <josh.chen.jia@gmail.com>
2018-08-09 23:40:01 -0700

    add card style

--- react-client/src/components/Websites.jsx
+++ react-client/src/components/Websites.jsx
@@ -30,9 +30,9 @@ class Websites extends React.Component {
 			<span>
 				<div className="jumbotron">
 					<div className="container">
-						<h1 className="display-3">Hello, world!</h1>
-						<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-						<p>Total reviews {this.state.websites.length}</p>						
+						<h1 className="display-3">Welcome to IMDb for Web app</h1>
+						<p>Grade and review your favorite Web app</p>
+						<p>Total {this.state.websites.length} websites</p>						
 					</div>
 				</div>							
 				<div className="container">
