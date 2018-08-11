f6b417ff5cfbba0a32a9cc7202f8e33d742a8347
montekaka <josh.chen.jia@gmail.com>
2018-08-11 15:12:28 -0700

    add moment to calculate create time from now

--- react-client/src/components/ReviewItem.jsx
+++ react-client/src/components/ReviewItem.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import moment from 'moment';
 
 class ReviewItem extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
 			review: props.review
-		}	
+		}
+		this.createdDateMoment = this.createdDateMoment.bind(this);
 	}	
+
+	createdDateMoment() {
+		var createDate = this.state.review.createdDate;
+		return moment(createDate).fromNow();
+	}
 	
 	render() {
 		return (
 			<div className="blog-post">
 			 	<h2 className="blog-post-title">{this.state.review.screenname}</h2>
 			 	<div>{this.state.review.rating} Stars</div>
-			 	<p className="blog-post-meta">January 1, 2014</p>			 	 
+			 	<p className="blog-post-meta">{this.createdDateMoment()}</p>			 	 
 			 	<p>{this.state.review.text}</p>			
 			</div>
 		)
